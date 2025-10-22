bc3c35b3d9cd253f4c82d210a97577a096026538
JimcostDev <rjak727@gmail.com>
2025-10-22 12:19:23 +0200

    update dependecies

--- src/components/ReportsList.jsx
+++ src/components/ReportsList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { fetchReports } from "../utils/api";
+import { fetchReports } from "@utils/api";
 import Title from "./Title";
 
 export default function ReportsList() {
